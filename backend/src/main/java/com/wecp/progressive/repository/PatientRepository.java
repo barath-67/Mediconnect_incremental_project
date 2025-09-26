651a73db6fb499411c0b810b97480871641485a1
barath-67 <101165503+barath-67@users.noreply.github.com>
2025-09-26 18:35:02 +0530

    Update PatientRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/PatientRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/PatientRepository.java
@@ -1,18 +1,22 @@
 package com.wecp.progressive.repository;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.wecp.progressive.entity.Patient;
+import java.util.List;
+
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
-        Patient findByPatientId(int patientId);
 
-    @Query("SELECT p FROM Patient p ORDER BY p.name ASC")
-    List<Patient> findAllPatientsSortedByName();
-    
+    // @Query("SELECT p FROM Patient p WHERE p.patientId = :patientId")
+    Patient findByPatientId(Integer patientId);
+
+    @Query("SELECT p FROM Patient p ORDER BY p.fullName ASC")
+    List<Patient> getAllPatientSortedByName();
 }
