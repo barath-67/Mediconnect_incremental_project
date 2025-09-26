1b083d0ad1d6c8dec5539af47e0bffe988b34bea
barath-67 <101165503+barath-67@users.noreply.github.com>
2025-09-26 18:36:59 +0530

    Update ClinicServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/ClinicServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/ClinicServiceImplJpa.java
@@ -11,55 +11,50 @@ import com.wecp.progressive.service.ClinicService;
 
 @Service
 public class ClinicServiceImplJpa implements ClinicService {
-
-    @Autowired
+    // @Autowired
     private ClinicRepository clinicRepository;
-
-    // Default constructor for Spring
-    public ClinicServiceImplJpa() {}
-    
-    // Constructor for testing
-    public ClinicServiceImplJpa(ClinicRepository clinicRepository) {
+    public ClinicServiceImplJpa(ClinicRepository clinicRepository){
         this.clinicRepository = clinicRepository;
     }
+
     @Override
-    public Integer addClinic(Clinic clinic) {
+    public Integer addClinic(Clinic clinic) throws Exception {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public void deleteClinic(int clinicId) {
+    public void deleteClinic(int clinicId) throws Exception {
         // TODO Auto-generated method stub
         
     }
 
     @Override
-    public List<Clinic> getAllClinicByDoctorId(int doctorId) {
+    public List<Clinic> getAllClinicByDoctorId(int doctorId) throws Exception {
         // TODO Auto-generated method stub
         return ClinicService.super.getAllClinicByDoctorId(doctorId);
     }
 
     @Override
-    public List<Clinic> getAllClinicByLocation(String location) {
+    public List<Clinic> getAllClinicByLocation(String location) throws Exception {
         // TODO Auto-generated method stub
         return ClinicService.super.getAllClinicByLocation(location);
     }
 
     @Override
-    public List<Clinic> getAllClinics() {
+    public List<Clinic> getAllClinics() throws Exception {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Clinic getClinicById(int clinicId) {
+    public Clinic getClinicById(int clinicId) throws Exception {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public void updateClinic(Clinic clinic) {
+    public void updateClinic(Clinic clinic) throws Exception {
         // TODO Auto-generated method stub
         
     }
