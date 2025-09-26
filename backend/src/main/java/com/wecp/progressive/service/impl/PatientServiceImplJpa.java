2ccef35667d209c04a9fe26c7428765b77d20001
barath-67 <101165503+barath-67@users.noreply.github.com>
2025-09-26 18:43:21 +0530

    Update PatientServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/PatientServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/PatientServiceImplJpa.java
@@ -6,58 +6,80 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wecp.progressive.dto.PatientDTO;
 import com.wecp.progressive.entity.Patient;
+import com.wecp.progressive.exception.PatientNotFoundException;
 import com.wecp.progressive.repository.PatientRepository;
 import com.wecp.progressive.service.PatientService;
 
 @Service
 public class PatientServiceImplJpa implements PatientService {
-    
+    // @Autowired
     private PatientRepository patientRepository;
-    @Autowired
+
     public PatientServiceImplJpa(PatientRepository patientRepository){
-        this.patientRepository=patientRepository;
+        this.patientRepository = patientRepository;
     }
 
-    public PatientServiceImplJpa(){}
-    Patient findByPatientId(int patientId){
-        return patientRepository.findByPatientId(patientId);
-    }
     @Override
-    public Integer addPatient(Patient patient) {
-        // TODO Auto-generated method stub
-       return patientRepository.save(patient).getPatientId();
-        
+    public Integer addPatient(Patient patient) throws Exception {
+        Patient p = patientRepository.save(patient);
+        return p.getPatientId();
     }
+
     @Override
-    public void deletePatient(int patientId) {
-        // TODO Auto-generated method stub
+    public void deletePatient(int patientId) throws PatientNotFoundException {
+        Optional<Patient> p = patientRepository.findById(patientId);
+        if(!p.isPresent()){
+            throw new PatientNotFoundException();
+        }
+
         patientRepository.deleteById(patientId);
     }
-    @Override
-    public List<Patient> getAllPatients() {
-        // TODO Auto-generated method stub
-        return patientRepository.findAll();
-    }
-    @Override
-    public void updatePatient(Patient patient) {
-        // TODO Auto-generated method stub
-        patientRepository.save(patient);
-    }
-    @Override
-    public List<Patient> getAllPatientSortedByName() {
-        // TODO Auto-generated method stub
-        return patientRepository.findAllPatientsSortedByName();
 
-    }
     @Override
-    public Patient getPatientById(int patientId) {
-        // TODO Auto-generated method stub
-        return patientRepository.findByPatientId(patientId);
+    public void emptyArrayList() throws Exception {
+        PatientService.super.emptyArrayList();
     }
 
-    
+    @Override
+    public List<Patient> getAllPatientSortedByName() throws Exception {
+       return patientRepository.getAllPatientSortedByName();
+    }
 
+    @Override
+    public List<Patient> getAllPatients() throws Exception {
+        List<Patient> li = patientRepository.findAll();
+        return li;
+    }
 
+    @Override
+    public Patient getPatientById(int patientId) throws Exception {
+        Patient p = patientRepository.findByPatientId(patientId);
+        return p;
+    }
+
+    @Override
+    public void updatePatient(Patient patient) throws Exception {
+
+         
+        Patient p = patientRepository.findByPatientId(patient.getPatientId());
+        p.setFullName(patient.getFullName());
+        p.setEmail(patient.getEmail());
+        p.setDateOfBirth(patient.getDateOfBirth());
+        p.setContactNumber(patient.getContactNumber());
+        p.setAddress(patient.getAddress());
+
+        patientRepository.save(p);
+
+        // TODO Auto-generated method stub
+        // PatientService.super.updatePatient(patient);
+    }
+
+    // @Override
+    // public void modifyPatientDetails(PatientDTO patientDTO) {
+    // // TODO Auto-generated method stub
+    // PatientService.super.modifyPatientDetails(patientDTO);
+    // }
 
 }
