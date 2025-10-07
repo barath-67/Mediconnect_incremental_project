ee85ae89286bd5408af514bd4a239d08b46b5eb8
username <wecp-dummy@gmail.com>
2025-10-07 18:12:41 +0000

    day16

--- frontend/src/app/mediconnect/models/Clinic.ts
+++ frontend/src/app/mediconnect/models/Clinic.ts
@@ -1,4 +1,28 @@
 
-export class Clinic {
-  
+export class Clinic 
+{
+    clinicId:number;
+    clinicName:string;
+    location:string;
+    doctorId:number;
+    contactNumber:string;
+    establishedYear:number;
+    constructor(clinicId:number,clinicName:string, location:string, doctorId:number, contactNumber:string, establishedYear:number)
+    {
+        this.clinicId=clinicId;
+        this.clinicName=clinicName;
+        this.location=location;
+        this.doctorId=doctorId;
+        this.contactNumber=contactNumber;
+        this.establishedYear=establishedYear;
+    }
+    logAttributes()
+    {
+        console.log('clinicId:', this.clinicId);
+        console.log('clinicName:', this.clinicName);
+        console.log('location:', this.location);
+        console.log('doctorId:', this.doctorId);
+        console.log('contactNumber:', this.contactNumber);
+        console.log('establishedYear:', this.establishedYear);
+    }
 }
